ed1e8b418ff7df3aa26b4aa8e80419e39befb07b
WTnickk <69070826+WTnickk@users.noreply.github.com>
2021-03-06 12:03:37 -0500

    Update createaccount.php

--- createaccount.php
+++ createaccount.php
@@ -1,1 +1,11 @@
-
+<?php
+    $username = $_GET['name'];
+    $password = $_GET['pwd'];
+    $filename = 'accounts.txt';
+    $fp = fopen($filename, 'a+');
+    fwrite ($fp, $username . "," . $password . "\n");
+    $fclose ($fp);
+    echo ("account created");
+    header("Location: "login.html"); 
+    die();
+?>
